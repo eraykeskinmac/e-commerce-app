c4a654f33d045f7b1c3aa64fa88eec51ef2f4ff2
Your Name <you@example.com>
2021-09-11 16:32:49 +0300

    ui: added card to basket page

--- client/src/pages/Basket/index.js
+++ client/src/pages/Basket/index.js
@@ -1,4 +1,13 @@
-import { Alert, Box, Button, Image, Text } from '@chakra-ui/react';
+import {
+  Alert,
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Image,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useBasket } from '../../contexts/BasketContext';
@@ -6,7 +15,7 @@ import { useBasket } from '../../contexts/BasketContext';
 function Basket() {
   const { items, removeFromBasket } = useBasket();
   const total = items.reduce((acc, obj) => acc + obj.price, 0);
-
+  console.log(items);
   return (
     <Box p="10">
       {items.length < 1 && (
@@ -15,34 +24,45 @@ function Basket() {
 
       {items.length > 0 && (
         <>
-          <ul>
+          <Grid templateColumns="repeat(3, 1fr)" gap={6}>
             {items.map((item) => (
-              <li key={item._id} style={{ margin: 20 }}>
+              <Box
+                key={item._id}
+                w="400px"
+                overflow="hidden"
+                boxShadow="sm"
+                bg="gray.200"
+              >
                 <Link to={`/products/${item._id}`}>
-                  {item.title} - {item.price} TL
                   <Image
-                    htmlWidth={200}
                     loading="lazy"
                     src={item.photos[0]}
                     alt="basket item"
                   />
                 </Link>
-                <Button
-                  mt="2"
-                  size="sm"
-                  colorScheme="pink"
-                  onClick={() => removeFromBasket(item._id)}
-                >
-                  Remove from basket
-                </Button>
-              </li>
+                <Stack p={2} isInline align="center" justify="space-between">
+                  <Text fontSize="1.5rem" fontWeight={400}>
+                    {item.title}
+                  </Text>
+                  <Text fontSize="1rem">{item.price} TL</Text>
+                </Stack>
+                <Flex justifyContent="end" p="2">
+                  <Button
+                    size="sm"
+                    colorScheme="pink"
+                    onClick={() => removeFromBasket(item._id)}
+                  >
+                    Remove
+                  </Button>
+                </Flex>
+              </Box>
             ))}
-          </ul>
+          </Grid>
         </>
       )}
 
       <Box mt="10">
-        <Text fontSize="22">Total: {total}</Text>
+        <Text fontSize="22">Total: {total} TL</Text>
       </Box>
     </Box>
   );
